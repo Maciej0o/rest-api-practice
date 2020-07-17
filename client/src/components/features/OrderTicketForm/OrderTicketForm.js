6dd98bcfcf8498d8af4f19712872a05313b35b7a
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-07-17 08:34:52 +0200

    Fix

--- client/src/components/features/OrderTicketForm/OrderTicketForm.js
+++ client/src/components/features/OrderTicketForm/OrderTicketForm.js
@@ -93,7 +93,7 @@ class OrderTicketForm extends React.Component {
             </FormGroup>
             <FormGroup check>
               <Label check>
-                <Input required type="checkbox" /> I agree with <a href="#">Terms and conditions</a> and <a href="#">Privacy Policy</a>.
+                <Input required type="checkbox" /> I agree with <i href="#">Terms and conditions</i> and <i href="#">Privacy Policy</i>.
               </Label>
             </FormGroup>
             <Button color="primary" className="mt-3">Submit</Button>
